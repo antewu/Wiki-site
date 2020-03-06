fb7a92522644dee76f850df1aec14a9d9eef3272
linyuxuanlin <824676271@qq.com>
2020-03-06 15:40:31 +0800

    1

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    title: 'Power\'s Wiki',
+
+    base: '/Wiki-vp/',
+    themeConfig: {
+
+
+        //navbar: false,
+        nav: [{
+                text: 'Home',
+                link: '/'
+            },
+            {
+                text: 'Guide',
+                link: '/guide/'
+            },
+            {
+                text: 'External',
+                link: 'https://google.com'
+            },
+        ],
+        sidebar: [
+            '/'
+        ]
+    }
+}
